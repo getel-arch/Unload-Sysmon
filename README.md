9767c52abb98302e538a1a40006495c574ffea64
getel-arch <getelmusic@gmail.com>
2025-05-18 11:32:17 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Unload-Sysmon
 
+[![Build & Release](https://github.com/getel-arch/Unload-Sysmon/actions/workflows/build_and_release.yaml/badge.svg)](https://github.com/getel-arch/Unload-Sysmon/actions/workflows/build_and_release.yaml)
--- src/main.c
+++ src/main.c
@@ -1,1 +1,33 @@
+#include <windows.h>
+#include <stdio.h>
+
+typedef void (*PFN_UNLOAD)();
+
+int main() {
+    // Load the Sysmon driver DLL
+    HMODULE hSysmonDrv = LoadLibraryA("sysmondrv");
+    if (!hSysmonDrv) {
+        printf("Failed to load sysmondrv. Error: %lu\n", GetLastError());
+        return 1;
+    }
+    printf("Loaded sysmondrv at 0x%p\n", hSysmonDrv);
+
+    // Get the address of the unload function
+    PFN_UNLOAD pfnUnload = (PFN_UNLOAD)GetProcAddress(hSysmonDrv, "DriverUnload");
+    if (!pfnUnload) {
+        printf("Failed to get address of DriverUnload. Error: %lu\n", GetLastError());
+        FreeLibrary(hSysmonDrv);
+        return 1;
+    }
+    printf("Address of DriverUnload: 0x%p\n", pfnUnload);
+
+    // Call the unload function
+    printf("Calling DriverUnload...\n");
+    pfnUnload();
+    printf("DriverUnload called.\n");
+
+    FreeLibrary(hSysmonDrv);
+
+    return 0;
+}
 
